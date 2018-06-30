d76410c192696742ccc9ba313b637a65a8c264e5
Jim Kyle <jim@jwkyle.com>
2018-06-30 16:46:30 -0500

    add routermodule.forChild routes in productmodule, edit out of appmodule

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -6,12 +6,20 @@ import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { StarComponent } from '../shared/star.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ProductGuardService } from './product-guard.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        canActivate: [ProductGuardService],
+        component: ProductDetailComponent
+      },
+    ])
   ],
   declarations: [
     ProductListComponent,
