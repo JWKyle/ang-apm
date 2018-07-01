32af03de03a76160fe2ea59edebc7b40d1e6e109
Jim Kyle <jim@jwkyle.com>
2018-07-01 13:49:25 -0500

    add getProduct method to product service and product component, edit imports, functional

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,26 +10,38 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Details';
+  errorMessage: string;
   product: IProduct;
 
   constructor(private _route: ActivatedRoute,
-            private _router: Router) {
+            private _router: Router,
+            private _productService: ProductService) {
 
    }
 
   ngOnInit() {
-    let id= +this._route.snapshot.paramMap.get('id');
-    this.pageTitle += ` ${id}`;
-    this.product = {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    const param = this._route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
     }
+    // let id= +this._route.snapshot.paramMap.get('id');
+    // this.pageTitle += ` ${id}`;
+    // this.product = {
+    //   "productId": 1,
+    //   "productName": "Leaf Rake",
+    //   "productCode": "GDN-0011",
+    //   "releaseDate": "March 19, 2016",
+    //   "description": "Leaf rake with 48-inch wooden handle.",
+    //   "price": 19.95,
+    //   "starRating": 3.2,
+    //   "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
+
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
   }
 
   onBack(): void {
